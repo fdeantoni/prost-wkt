d7eb9ec8027bd8fce112c7c37a4ddc976ed44149
fdeantoni <fdeantoni@gmail.com>
2021-11-26 12:14:43 +0800

    Extract timestamp methods from prost-types.

--- wkt-types/build.rs
+++ wkt-types/build.rs
@@ -2,11 +2,16 @@ use std::env;
 use std::fs::create_dir_all;
 use std::path::{Path, PathBuf};
 
+use std::fs::File;
+use std::io::Write;
+
 use prost::Message;
 use prost_types::FileDescriptorSet;
 
 fn main() {
+
     let dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    download_prost_pbtime(&dir);
 
     build(&dir, "pbtime");
     build(&dir, "pbstruct");
@@ -38,4 +43,19 @@ fn build(dir: &Path, proto: &str) {
     let descriptor = FileDescriptorSet::decode(&descriptor_bytes[..]).unwrap();
 
     prost_wkt_build::add_serde(out, descriptor);
+}
+
+fn download_prost_pbtime(dir: &Path) {
+    let url = "https://raw.githubusercontent.com/tokio-rs/prost/v0.9.0/prost-types/src/lib.rs";
+    let resp = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let lines: Vec<String> = resp.lines().map(|s| s.to_string()).collect();
+    let selection = &lines[27..256];
+    let mut string = String::new();
+    for line in selection {
+        string.push_str(line);
+        string.push('\n');
+    }
+
+    let file = dir.join("prost_snippet.rs");
+    File::create(file).unwrap().write_all(string.as_bytes()).unwrap();
 }
